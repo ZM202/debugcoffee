9c26266df25f7a858062dbb149335f9916cb7ab7
ZM <48497724+ZM202@users.noreply.github.com>
2020-03-14 08:24:26 -0400

    Add files via upload

---
+++ Debug-Your-Coffee/script.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    let cupsDrank, response;
+    cupsDrank = document.getElementById("cupsDrank").value;
+    
+    if (cupsDrank < 4) {
+        response = "You are fine, maybe have another cup!"
+        document.getElementById("result").innerHTML = response;
+        console.log(cupsDrank);
+    } else if (cupsDrank >= 4) {
+        response = "Run to the bathroom NOW!";  
+        document.getElementById("result").innerHTML = response;
+        console.log(cupsDrank);
+    }
+
+    document.getElementById("clyde");
+    console.log(document.getElementById("clyde").innerHTML);
+
+    console.log(response);
+    
+}
+
